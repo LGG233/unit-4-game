c6a51836e57cc6cd3be859261d60d4a5c09cf846
Fred Astaire <lancebd@thegodardgroup.com>
2019-02-18 01:21:27 -0500

    Almost there

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,3 @@
-//Functions
-// resetGame
-// rollDice
-// addPoints
-// runningTotal
-// reportStatus
-// autoRestart
-// use to kick-off game: window.onload = function () { 
-
-//Variables
 var statusReport = "";
 var gamesWon = 0;
 var gamesLost = 0;
@@ -18,41 +8,98 @@ var orangeCrystal = 0;
 var purpleCrystal = 0;
 var pinkCrystal = 0;
 
+$(document).ready(function () {
+  $("#totalScore").text(totalScore);
+  $("#gamesWon").text(gamesWon);
+  $("#gamesLost").text(gamesLost);
+  $("#statusReport").html("<h2>Here we go!</h2>");
+  pointsToMatch();
+  rollDice();
+  assignPoints();
+  scoreBoard();
 
-// Pseudocode
-// 1. on game start, reset everything to zero -- function name: resetGame
-//  This code will run as soon as the page loads.
-function resetGame() {
-    statusReport = "";
+  function randomNumber(min = 1, max = 12) {
+    return Math.floor(Math.random() * max) + min;
+  }
+
+  function delayedReset () {
+    setTimeout(resetGame, 3000);
+  }
+
+  function resetGame() {
     totalScore = 0;
-    $("#statusReport").append("<h2>Here we go!</h2>");
+    gamePoints = 0;
+    redCrystal = 0;
+    orangeCrystal = 0;
+    purpleCrystal = 0;
+    pinkCrystal = 0;
+    $("#totalScore").text(totalScore);
+    $("#statusReport").html("<h2>Here we go!</h2>");
+    pointsToMatch();
     rollDice();
+    scoring();
+  };
 
-}
+  function pointsToMatch() {
+    gamePoints = randomNumber(19, 101);
+    $("#gamePoints").text(gamePoints);
+    console.log("Game points are: " + gamePoints)
+  };
 
-// 2. "Roll the dice" - put random numbers under the crystals and also assign gamePoints -- function name: rollDice
-function rollDice() {
-    redCrystal = Math.floor(Math.random() * 12);
-    orangeCrystal = Math.floor(Math.random() * 12);
-    purpleCrystal = Math.floor(Math.random() * 12);
-    pinkCrystal = Math.floor(Math.random() * 30);
-}
-// 3. Assign random points to each crystal: function name: assignPoints
-function assignPoints() {
-    $("#redCry").on("click", redCrystal);
-    $("#orangeCry").on("click", orangeCrystal);
-    $("#purpleCry").on("click", purpleCrystal);
-    $("#pinkCry").on("click", pinkCrystal);
-}
+  function rollDice() {
+    redCrystal = randomNumber();
+    orangeCrystal = randomNumber();
+    purpleCrystal = randomNumber();
+    pinkCrystal = randomNumber();
+    console.log("Red is: " + redCrystal);
+    console.log("Orange is: " + orangeCrystal);
+    console.log("Purple is: " + purpleCrystal);
+    console.log("Pink is: " + pinkCrystal);
+  };
 
-// 3.5 Put click event on each crystal that adds the points under each crystal to total score -- function name: addPoints
+  function assignPoints() {
+    $("#redCry").click(function() {
+      this.value = redCrystal;
+      totalScore = totalScore + redCrystal;
+      $("#totalScore").text(totalScore);
+      scoreBoard();
+    });
+    $("#orangeCry").click(function() {
+      this.value = orangeCrystal;
+      totalScore = totalScore + orangeCrystal;
+      $("#totalScore").text(totalScore);
+      scoreBoard();
+    });
+    $("#purpleCry").click(function() {
+      this.value = purpleCrystal;
+      totalScore = totalScore + purpleCrystal;
+      $("#totalScore").text(totalScore);
+      scoreBoard();
+    });
+    $("#pinkCry").click(function() {
+      this.value = pinkCrystal;
+      totalScore = totalScore + pinkCrystal;
+      $("#totalScore").text(totalScore);
+      scoreBoard();
+    });
+  };
 
-// 4. Compare running total (#totalScore) to game points (#gamePoints) -- function name: runningTotal
-function runningTotal() {
-    
-}
-// 5. alert player to win / loss -- function name: reportStatus 
-// 6. create automatic restart which rolls the dice but doesn't reset to zero wins or losses -- function name: autoRestart
-// NB: Game points is random number between 19 and 120
-// NB: Crystal points are random numbers between 1 and 12
+  function scoreBoard() {
+    if (totalScore === gamePoints) {
+      gamesWon = gamesWon + 1;
+      $("#statusReport").html("<h2>Yippee! You won!</h2>");
+      $("#gamesWon").text(gamesWon);
+      delayedReset();
+    } else if (totalScore > gamePoints) {
+      gamesLost = gamesLost + 1;
+      $("#statusReport").html("<h2>Nice try but you lose</h2>");
+      $("#gamesLost").text(gamesLost);
+      delayedReset();
+    }
+  }
 
+  function scoring() {
+    assignPoints();
+    scoreBoard();
+  }
+})
